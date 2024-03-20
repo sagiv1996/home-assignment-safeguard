de750cd8fea150d1ab061f803f78d87de8951b9c
Sagiv <sagivb13@gmail.com>
2024-03-20 17:30:57 +0200

    create a background for home screen

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,9 +7,72 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("Titlte"),
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(children: [
+            Align(
+                alignment: const AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: MediaQuery.sizeOf(context).width,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.rectangle, color: Colors.amber),
+                )),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 250,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                )),
+            Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: 250,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                )),
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 100,
+                sigmaY: 100,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.transparent),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "TEST",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w300),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    "Good Morning",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            )
+          ]),
         ),
-        body: Text("BODY!"),
-      );
+      ));
 }
