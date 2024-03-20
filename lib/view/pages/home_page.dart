c0f9d48320e605e8982dc8b2ec82ff0a1939b64c
Sagiv <sagivb13@gmail.com>
2024-03-20 16:36:10 +0200

    install and use with goRouter

---
+++ lib/view/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Titlte"),
+        ),
+        body: Text("BODY!"),
+      );
+}
